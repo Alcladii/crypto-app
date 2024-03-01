eb6873a1d028e3d27a6c36c0aa9b443a5e84359e
Alcladii <122701312+Alcladii@users.noreply.github.com>
2024-02-29 18:52:06 -0800

    fixed switching between top 50 and bottom 50 bugs

--- src/components/Coins.jsx
+++ src/components/Coins.jsx
@@ -83,7 +83,7 @@ function Coins() {
     "displayCoinList",
     []
   );
-
+  
   const showTopFifty = queryParams.show_top_fifty === "true";
 
   const getCoinList = async () => {
@@ -99,7 +99,7 @@ function Coins() {
         "/coins/markets",
         "vs_currency=usd&order=market_cap_desc&per_page=50&page=1&sparkline=true&price_change_percentage=1h%2C24h%2C7d"
       );*/
-      let coins;
+      let coins;     
       const order = showTopFifty ? "market_cap_desc" : "market_cap_asc";
       setCoinListIsLoading(true);
       const response = await api(
@@ -128,7 +128,7 @@ function Coins() {
   };
 
   useEffect(() => {
-    handleSearchParams("show_top_fity", coinListDsc);
+    handleSearchParams("show_top_fifty", coinListDsc);
   }, [coinListDsc]);
 
   const options = {
@@ -171,7 +171,7 @@ function Coins() {
     }, minute);
 
     return () => clearInterval(intervalId);
-  }, [coinListDsc]);
+  }, [coinListDsc, showTopFifty]);
 
   useEffect(() => {
     getCurrencyList();
