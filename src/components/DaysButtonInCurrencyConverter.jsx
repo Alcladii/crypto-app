d7ff4d7d2318df71dd77c499e7ef90be1006d957
Alcladii <122701312+Alcladii@users.noreply.github.com>
2024-03-03 01:26:28 -0800

    temp save 3

--- src/components/DaysButtonInCurrencyConverter.jsx
+++ src/components/DaysButtonInCurrencyConverter.jsx
@@ -1,1 +1,24 @@
+import React, { useContext } from "react";
+import "../App.css";
+import { CryptoContext } from "../contexts/cryptoContext";
+
+export const DaysButtonInCurrencyConverter = ({ days, buttonText }) => {
+  const { currencyConverterDays, setCurrencyConverterDays } =
+    useContext(CryptoContext);
+
+  return (
+    <div>
+      <div
+        className={`${
+          currencyConverterDays === days ? "selected-button" : "bg-transparent"
+        } w-16 h-10 mx-1 my-1  flex justify-center items-center rounded-md cursor-pointer`}
+        onClick={() => {
+          setCurrencyConverterDays(days);
+        }}
+      >
+        {buttonText}
+      </div>
+    </div>
+  );
+};
 
