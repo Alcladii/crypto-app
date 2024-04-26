fcf90b95465d87345d96228790ee9f477844ab1a
Alcladii <122701312+Alcladii@users.noreply.github.com>
2024-04-25 17:43:04 -0700

    deleted extra empty line in ResultList.jsx

--- src/components/ResultList.jsx
+++ src/components/ResultList.jsx
@@ -9,7 +9,6 @@ export const ResultList = ({results}) => {
     history.push(`/coin-page/${item.id}`);
   };
   
-
   return (  
     <div className={`${results.length === 0 ? "hidden" : "absolute"} top-[230%] lg:top-[100%] -left-24 md:left-0 flex flex-col w-56 px-2.5 py-1 lg:w-[100%] rounded-md shadow-md shadow-color-gray-300 mt-1 max-h-40 overflow-y-scroll no-scrollbar text-skin-coin-search-result-list-text-color cursor-pointer bg-skin-coin-search-result-list-background-color`}>
       {results.map((item) => (
